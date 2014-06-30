09dd24693057800153132dae61604af1af946dfe
Vladislav Semyonoff <vsemyonoff@icloud.com>
2014-06-30 10:48:10 +0200

    Initial commit

---
+++ include/main.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+class Main {
+    public:
+        static int run() {
+            std::cout << "Hello, World!!!\n";
+            return 0;
+        }
+};
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include "main.hpp"
+
+int main(int argc, const char** argv) {
+    return Main::run();
+}
