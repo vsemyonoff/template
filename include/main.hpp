d639e2d6c82433c13d4089748978d46915b053c7
Vladislav Semyonoff <vsemyonoff@icloud.com>
2014-06-30 12:45:36 +0200

    Implementation updated

--- include/main.hpp
+++ include/main.hpp
@@ -1,11 +1,6 @@
 #pragma once
 
-#include <iostream>
-
 class Main {
-    public:
-        static int run() {
-            std::cout << "Hello, World!!!\n";
-            return 0;
-        }
+public:
+    static int run();
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "main.hpp"
 
+#include <iostream>
+
+int Main::run() {
+    std::cout << "HELLo, World!!!\n";
+    return 0;
+}
+
 int main(int argc, const char** argv) {
     return Main::run();
 }
